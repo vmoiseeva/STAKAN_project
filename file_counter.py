440754ba2076b0027c60411e350c3cbf821c0eb6
vmoiseeva <116632508+vmoiseeva@users.noreply.github.com>
2023-09-30 21:07:06 +0300

    changed the location of the time counter

--- file_counter.py
+++ file_counter.py
@@ -4,7 +4,6 @@ import time
 
 
 def count_files(path):
-    start_time = time.time()
     file_counter = 0  # заводим счетчик для подсчета файлов
 
     for root, _, files in os.walk(path):
@@ -13,13 +12,14 @@ def count_files(path):
             if isfile(file_path):  # проверяем, является ли файлом рассматриваемый элемент
                 file_counter += 1  # считаем его, если да
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    return file_counter, elapsed_time
+    return file_counter
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     path = os.path.abspath(os.sep)  # получаем доступ к корневой папке
-    total_files, elapsed_time = count_files(path)
+    total_files = count_files(path)
+    end_time = time.time()
+
     print(f"Total files on your hard drive: {total_files}")
+    print(f"Program execution time:{end_time - start_time:.2f} seconds")
