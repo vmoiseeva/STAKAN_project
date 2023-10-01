a8b2f47554be237e22e98c3c2779bde2986d2527
vmoiseeva <116632508+vmoiseeva@users.noreply.github.com>
2023-10-01 12:05:41 +0300

    file_counter using csv

--- file_counter.py
+++ file_counter.py
@@ -1,24 +1,38 @@
 import os
-from os.path import join, isfile
 import time
+import pandas as pd
 
 
-def count_files(path):
-    file_counter = 0  # заводим счетчик для подсчета файлов
+def count_files(path, csv_file):
+    file_counter = 0
 
-    for root, _, files in os.walk(path):
-        for filename in files:
-            file_path = join(root, filename)
-            if isfile(file_path):  # проверяем, является ли файлом рассматриваемый элемент
-                file_counter += 1  # считаем его, если да
+    # Check if the CSV file exists
+    if os.path.exists(csv_file):
+        # Get the file's modification time
+        file_modification_time = os.path.getmtime(csv_file)
+
+        # Calculate the current time
+        current_time = time.time()
+
+        # Check if the CSV file was modified more than two days ago
+        if current_time - file_modification_time > 2 * 24 * 3600:
+            print("Warning: The CSV file should be updated.")
+
+        # Read the CSV file using pandas
+        df = pd.read_csv(csv_file)
+
+        # Count the number of rows (files) in the CSV
+        file_counter = len(df)
 
     return file_counter
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    path = os.path.abspath(os.sep)  # получаем доступ к корневой папке
-    total_files = count_files(path)
+    path = '/Users/valeriiamoiseeva/PycharmProjects/STAKAN_project'
+    csv_file = 'file_data.csv'
+
+    total_files = count_files(path, csv_file)
     end_time = time.time()
 
     print(f"Total files on your hard drive: {total_files}")
